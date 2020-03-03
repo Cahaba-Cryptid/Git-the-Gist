87fa887132e98b09e6c97bac22834b30d91b9587
Hayden <chfrank9011@gmail.com>
2020-03-03 11:56:37 -0600

    Added a for-each loop in the add-gist .hbs file to iterate over test gists and display them in a set format. Set add-gist controller up with dummy data. Changed HTML title to new punny version.

--- app/controllers/add-gist.js
+++ app/controllers/add-gist.js
@@ -1,5 +1,28 @@
 import Controller from '@ember/controller';
 
 export default class AddGistController extends Controller {
-  testValue = "hello"
+  //Dummy data
+  gists = [
+    {
+      fileName: "Example .hbs file",
+      description: "An example of a simple .hbs file.",
+      body: "<p>An <strong>example<strong/> for {{username}}</p>"
+    }, 
+    {
+      fileName: "Example JavaScript fuction",
+      description: "This is a simple JS function that will print to the console 'Hello World'",
+      body: `function sayHello() {
+              console.log("Hello World")
+            };
+            
+            sayHello();
+            `
+    },
+    {
+      fileName: "Lazy example",
+      description: "Super lazy description",
+      body: "<h1>I'm lazy.</h1>"
+    }
+  ]
 }
+
