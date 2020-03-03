be486a8c777698e658606deee80dc57495f33c5c
Hayden <chfrank9011@gmail.com>
2020-03-03 13:39:32 -0600

    Added some functionality with a snap button that splits the shown gists in half using a modulus.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,23 @@
 import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
+
 
 export default class IndexController extends Controller {
   @service gistsInfo;
+  @tracked snapped = false;
+
+  get shownGists() {
+    if(this.snapped) {
+      //filter snapped objects
+      return this.gistsInfo.gists.filter((item, index) => index % 2 === 0) 
+    } else {
+     return this.gistsInfo.gists
+    }
+  }
+
+  @action snap() {
+    this.snapped = !this.snapped;
+  }
 }
