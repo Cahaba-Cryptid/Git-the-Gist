29f68de7ca902a06b591b39534afcf7e5906970a
Hayden <chfrank9011@gmail.com>
2020-03-03 13:13:39 -0600

    Added index route and controller for use on home page. It will display all gists in the service.

--- app/components/add-gist.js
+++ app/components/add-gist.js
@@ -1,12 +1,5 @@
 import Component from '@glimmer/component';
-import {action} from "@ember/object";
-import {inject as service} from "@ember/service";
 
 export default class AddGistComponent extends Component {
-  @service gistsInfo;
-
-  @action
-  addFile(body, description, fileName) {
-    this.gistsInfo.addFile({body, description, fileName});
-  }
+  
 }
--- app/controllers/add-gist.js
+++ app/controllers/add-gist.js
@@ -1,6 +1,14 @@
 import Controller from '@ember/controller';
+import {action} from "@ember/object";
+import {inject as service} from "@ember/service";
 
 export default class AddGistController extends Controller {
-  
+  @service gistsInfo;
+
+  @action
+  addFile(body, description, fileName) {
+    this.gistsInfo.addGist({body, description, fileName});
+    this.transitionToRoute('index');
+  }
 }
 
